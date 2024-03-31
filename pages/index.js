eb7f0e794df8fa0de66b965e5e203c02b286ba8f
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2024-03-30 23:40:20 -0400

    Feat. A close look at getStaticProps and Configuration options

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,30 @@ function HomePage(props) {
 }
 
 // This function gets called at build time
-export async function getStaticProps() {
-  console.log('Re-generating...');
+// context contains various properties, such as the route (params) and preview (preview)
+export async function getStaticProps(context) {
+  console.log("Re-generating...");
   // process cwd is the current working directory, which is the root of the project
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      // 302 is the default status code
+      redirect: {
+        // destination is the path to redirect to
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      // notFound: true will cause a 404 page to be rendered
+      notFound: true,
+    };
+  }
 
   // By returning { props: products }, the HomePage component
   // will receive `products` as a prop at build time
